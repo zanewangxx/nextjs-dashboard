cf33bf7078c7908c5feacb8140390b66eeaeddb6
Zhen Wang <wangzhenaph@gmail.com>
2024-06-29 16:41:38 +1000

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+import { Lusitana } from 'next/font/google';
+
+// Initialize the font
+export const lusitana = Lusitana({
+  subsets: ['latin'], // You can specify the subsets you need
+  weight: ['400', '700'], // You can specify the weights you need
+});
